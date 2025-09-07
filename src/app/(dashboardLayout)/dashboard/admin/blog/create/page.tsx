2bb9226a6c7f875fe97fa76d31bc8065fb9e353c
md-suhag <abdussalamsuhag104@gmail.com>
2025-09-07 14:04:53 +0600

    add: create new blog page design

--- src/app/(dashboardLayout)/dashboard/admin/blog/create/page.tsx
+++ src/app/(dashboardLayout)/dashboard/admin/blog/create/page.tsx
@@ -1,9 +1,144 @@
-import React from 'react'
+"use client";
+import QuillEditor from "@/components/modules/blog/QuillEditor";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+const blogSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  category: z.string().min(1, "Category is required"),
+  image: z.any().refine((files) => files?.length == 1, "Image is required."),
+});
 
 const CreateBlog = () => {
-  return (
-    <div>CreateBlog</div>
-  )
-}
+  const form = useForm<z.infer<typeof blogSchema>>({
+    resolver: zodResolver(blogSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      category: "",
+      image: undefined,
+    },
+  });
 
-export default CreateBlog
+  const onSubmit = (data: z.infer<typeof blogSchema>) => {
+    const formData = new FormData();
+    console.log(data);
+    const { image, ...blogData } = data;
+
+    formData.append("data", JSON.stringify(blogData));
+    formData.append("image", image[0]);
+  };
+
+  return (
+    <div className="w-full p-4">
+      <h1 className="text-3xl font-bold text-center">Create New Blog</h1>
+      <div className="mt-8">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter blog title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a category" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="Web_Development">
+                        Web Development
+                      </SelectItem>
+                      <SelectItem value="Graphic_Design">
+                        Graphic Design
+                      </SelectItem>
+                      <SelectItem value="Programming">Programming</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="image"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Image</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="file"
+                      onChange={(e) => field.onChange(e.target.files)}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Blog Content</FormLabel>
+                  <FormControl>
+                    <QuillEditor
+                      value={field.value}
+                      onChange={field.onChange}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBlog;
