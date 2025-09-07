befc80a2d22af622a44c9b88c74ab6744e7ab2a3
md-suhag <abdussalamsuhag104@gmail.com>
2025-09-07 07:55:42 +0600

    add: login page design

--- src/app/(mainLayout)/login/page.tsx
+++ src/app/(mainLayout)/login/page.tsx
@@ -1,7 +1,21 @@
+import { LoginForm } from "@/components/modules/auth/LoginForm";
+import Link from "next/link";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="bg-muted flex min-h-[90vh] flex-col items-center justify-center gap-4 p-4 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-4">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center text-lg font-medium"
+        >
+          Tech Sea
+        </Link>
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
