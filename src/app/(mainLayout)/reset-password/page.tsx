f2300eda2fc2f25443cc89eb6eab29c932ad4d75
md-suhag <abdussalamsuhag104@gmail.com>
2025-09-07 08:57:36 +0600

    add: reset password page

--- src/app/(mainLayout)/reset-password/page.tsx
+++ src/app/(mainLayout)/reset-password/page.tsx
@@ -1,7 +1,21 @@
+import { ResetPasswordForm } from "@/components/modules/auth/ResetPasswordForm";
+import Link from "next/link";
 import React from "react";
 
 const ResetPassword = () => {
-  return <div>ResetPassword</div>;
+  return (
+    <div className="bg-muted flex min-h-[90vh] flex-col items-center justify-center gap-4 p-4 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-4">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center text-lg font-medium"
+        >
+          Tech Sea
+        </Link>
+        <ResetPasswordForm />
+      </div>
+    </div>
+  );
 };
 
 export default ResetPassword;
---
+++ src/components/modules/auth/ResetPasswordForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Password from "@/components/ui/Password";
+
+const resetPasswordSchema = z
+  .object({
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Password must be at least 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export function ResetPasswordForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const form = useForm<z.infer<typeof resetPasswordSchema>>({
+    resolver: zodResolver(resetPasswordSchema),
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof resetPasswordSchema>) => {
+    try {
+    } catch (error) {}
+  };
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Reset Password</CardTitle>
+          <CardDescription>Enter your new password</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid gap-6">
+                <div className="grid gap-6">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>New Password</FormLabel>
+                        <FormControl>
+                          <Password {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Confirm New Password</FormLabel>
+                        <FormControl>
+                          <Password {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit" className="w-full">
+                    Submit
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
